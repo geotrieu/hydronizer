269b6b8bbf738e4f019453d83e1b92360c42c265
kseto2 <kseto2@edu.uwaterloo.ca>
2021-01-16 17:47:29 -0500

    clean up

--- hydronizer-website/src/components/Navbar/MenuItems.js
+++ hydronizer-website/src/components/Navbar/MenuItems.js
@@ -1,15 +1,5 @@
 export const MenuItems = [
     {
-        title: 'Sign Up',
-        url: 'Sign-Up',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Log In',
-        url: 'Log-in',
-        cName: 'nav-links'
-    },
-    {
         title: 'Sign Out',
         url: 'Sign-Out',
         cName: 'nav-links'
