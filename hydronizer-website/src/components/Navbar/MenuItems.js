87a582f4dc80197d6f8c7ca91b9152bda1b3363c
kseto2 <kseto2@edu.uwaterloo.ca>
2021-01-16 14:29:53 -0500

    Add Navbar

---
+++ hydronizer-website/src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
---
+++ hydronizer-website/src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems";
+import './Navbar.css'
+class Navbar extends Component {
+    state = { clicked: false }
+    
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Hydronizer<i className="fab fa-react"></i></h1>
+                <div className="menu-icon">
+
+                </div>
+                <ul className="nav-menu">
+                    {MenuItems.map((item, index) =>{
+                        return(
+                        <li key={index}>
+                            <a className={MenuItems.cName} href={item.url}>
+                            {item.title}
+                            </a>
+                        </li>)
+                    })}
+                    
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
