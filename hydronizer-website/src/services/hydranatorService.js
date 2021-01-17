340e6e7c7cd13b8b800ee07b88efe13cd6d9160b
geotrieu <trieugeorge@gmail.com>
2021-01-17 04:58:34 -0500

    removed windows port

--- hydronizer-website/src/services/hydranatorService.js
+++ hydronizer-website/src/services/hydranatorService.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 
+const serverAddress = "http://35.185.60.243:5000";
+
 export function getWaterLeft(device) {
-    return axios.get('http://localhost:5000/lastwaterbreak?deviceid=' + device);
+    return axios.get(serverAddress + '/lastwaterbreak?deviceid=' + device);
 }
 
 export function getMetrics(device) {
-    return axios.get('http://localhost:5000/metrics?deviceid=' + device);
+    return axios.get(serverAddress + '/metrics?deviceid=' + device);
 }
